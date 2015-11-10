32730cbd1410527665e4cdc556866b9b1697d66e
amdudda <tk0654wm@go.minneapolis.edu>
2015-11-10 14:53:53 -0600

    Converted priority setting to combobox in NewTicket screen.

--- src/com/amdudda/NewTicket.java
+++ src/com/amdudda/NewTicket.java
@@ -10,11 +10,13 @@ import java.time.LocalDateTime;
  */
 public class NewTicket extends JFrame {
     private JTextField addNewTicketTextField;
-    private JTextField priorityTextField;
     private JTextField reportedByTextField;
     private JTextArea descriptionTextArea1;
     private JButton addTicketToSystemButton;
     private JPanel rootPanel;
+    //private int[] priorityLevels = {1,2,3,4,5};
+    private JComboBox<Integer> priorityComboBox;
+
 
     public NewTicket() {
         super("Enter New Ticket");
@@ -23,14 +25,20 @@ public class NewTicket extends JFrame {
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setVisible(true);
 
+       // add values to the combo box and set default rating to 3.
+        for (int i=1; i<=5; i++) {
+            priorityComboBox.addItem(i);
+        }
+        priorityComboBox.setSelectedIndex(2);
+
         addTicketToSystemButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // gather our values to pass into ticketQueue
                 String descript = descriptionTextArea1.getText();
-                String priText = priorityTextField.getText();
+                int priInt = priorityComboBox.getSelectedIndex()+1;
                 String rept = reportedByTextField.getText();
-                int priInt = Integer.parseInt(priText);
+                //int priInt = Integer.parseInt(priText);
                 Ticket toAdd = new Ticket(descript, priInt, rept, LocalDateTime.now());
                 // add info as a new ticket in ticketQueue
                 TicketManager.ticketQueue.add(toAdd);
@@ -40,7 +48,4 @@ public class NewTicket extends JFrame {
         });
     }
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-    }
 }
